d4d6de91b2accbd49046cfea14e6a34f0ea80377
LorenzoRimella <lr17384@bristol.ac.uk>
2019-11-08 11:03:28 +0000

    Add prior computation inside the class

--- BayesianNetwork.py
+++ BayesianNetwork.py
@@ -115,6 +115,12 @@ class BayesianNetwork(nn.Module):
         self.architecture  = architecture
         self.depth         = self.architecture.shape[0]
 
+        self.alpha_k = alpha_k
+        self.sigma_k = sigma_k 
+        self.pi      = pi     
+        self.p       = p      
+        self.c       = c      
+
         self.Linear_layer  = nn.ModuleList()
 
         if BayesianNetwork_init == False:
@@ -152,106 +158,87 @@ class BayesianNetwork(nn.Module):
 
         return mu_stack, rho_stack, w_stack
 
+    # Prior
+    def get_gaussianlikelihood(self, x, mu, sigma):
+
+        return (1/(np.sqrt(2*np.pi)*sigma))*torch.exp(-((x-mu)*(x-mu))/(2*sigma*sigma))
+
+    ###############################################################################################
+
+    def get_gaussianlogkernelprior(self, x, mu_prev, sigma_prev, mu_new):
+        
+        with torch.no_grad():
+            mu_prev    = torch.tensor( mu_prev.data.numpy() )
+            sigma_prev = torch.tensor( sigma_prev.data.numpy() )
+                
+        mu1    = mu_new - self.alpha_k*mu_new + self.alpha_k*mu_prev
+        sigma1 = torch.sqrt(self.sigma_k*self.sigma_k + self.alpha_k*self.alpha_k*sigma_prev*sigma_prev)
+        f1= self.get_gaussianloglikelihood(x, mu1, sigma1)
+                
+        mu2    = mu_new - self.alpha_k*mu_new
+        sigma2 = torch.sqrt(self.sigma_k*self.sigma_k + self.alpha_k*self.alpha_k*sigma_prev*sigma_prev)       
+        f2= self.get_gaussianloglikelihood(x, mu2, sigma2)
+                
+        mu3    = mu_new - self.alpha_k*mu_new + self.alpha_k*mu_prev
+        sigma3 = torch.sqrt(self.sigma_k*self.sigma_k/(self.c*self.c) + self.alpha_k*self.alpha_k*sigma_prev*sigma_prev)       
+        f3= self.get_gaussianloglikelihood(x, mu3, sigma3)
+                
+        mu4    = mu_new - self.alpha_k*mu_new 
+        sigma4 = torch.sqrt(self.sigma_k*self.sigma_k/(self.c*self.c) + self.alpha_k*self.alpha_k*sigma_prev*sigma_prev)   
+        f4= self.get_gaussianloglikelihood(x, mu4, sigma4)
+
+        overall = self.pi*self.p*(f1) + self.pi*(1-self.p)*(f2) + (1-self.pi)*self.p*(f3) + (1-self.pi)*(1-self.p)*(f4)
+        summing = (torch.log(overall))
+        
+        return summing
+
+
+    ###############################################################################################
+    ###############################################################################################
+
+    def get_gaussianloglikelihood_qw(self, x, mu, sigma):
+
+        return -0.5*np.log(2*np.pi) + torch.log( (1-self.p)/sigma*torch.exp(- (x)*(x)/(2 * sigma*sigma)) + (self.p)/sigma*torch.exp(- (x - mu)*(x - mu)/(2 * sigma*sigma) ))
+
+
+    ##############################################################################################
+    # Overall likelihood, without the neural network part
+    ##############################################################################################
+    # Here we are using the previous variational approximation as approximate posterior
+
+    def get_gaussiandistancefromprior(self, mu_new, mu_prev, rho_prev):
+            
+        log_qw_theta_sum = 0
+        log_pw_sum       = 0
+        
+         w, mu, rho = self.stack()
+
+        sigma = torch.log1p( torch.exp( rho ) )
+
+        log_qw_theta = self.get_gaussianloglikelihood_qw(w, mu, sigma)
+        log_qw_theta_sum = (log_qw_theta).sum()
+
+
+        sigma_prev = torch.log1p( torch.exp( rho_prev ) ) 
+
+        log_pw     = self.get_gaussianlogkernelprior( w, mu_prev, sigma_prev, mu_new)
+        log_pw_sum = (log_pw).sum()
+
+
+        return (log_qw_theta_sum - log_pw_sum)
+
+
+
+
+
+
+
+
 
 
 ##############################################################################################
 # Define an Hidden Markov neural network
 ##############################################################################################
-
-# Collection of useful functions
-###############################################################################################
-
-def get_gaussianlikelihood(x, mu, sigma):
-
-    return (1/(np.sqrt(2*np.pi)*sigma))*torch.exp(-((x-mu)*(x-mu))/(2*sigma*sigma))
-
-###############################################################################################
-
-def combined(x, mu_prev, sigma_prev, alpha_k, sigma_k, mu_new, pi, p, c):
-    
-    with torch.no_grad():
-        mu_prev    = torch.tensor(mu_prev.data.numpy())
-        sigma_prev = torch.tensor(sigma_prev.data.numpy())
-             
-    mu1    = mu_new - alpha_k*mu_new + alpha_k*mu_prev
-    sigma1 = torch.sqrt(sigma_k*sigma_k + alpha_k*alpha_k*sigma_prev*sigma_prev)
-    f1= get_gaussianloglikelihood(x, mu1, sigma1)
-             
-    mu2    = mu_new - alpha_k*mu_new
-    sigma2 = torch.sqrt(sigma_k*sigma_k + alpha_k*alpha_k*sigma_prev*sigma_prev)       
-    f2= get_gaussianloglikelihood(x, mu2, sigma2)
-             
-    mu3    = mu_new - alpha_k*mu_new + alpha_k*mu_prev
-    sigma3 = torch.sqrt(sigma_k*sigma_k/(c*c) + alpha_k*alpha_k*sigma_prev*sigma_prev)       
-    f3= get_gaussianloglikelihood(x, mu3, sigma3)
-             
-    mu4    = mu_new - alpha_k*mu_new 
-    sigma4 = torch.sqrt(sigma_k*sigma_k/(c*c) + alpha_k*alpha_k*sigma_prev*sigma_prev)   
-    f4= get_gaussianloglikelihood(x, mu4, sigma4)
-
-    overall = pi*p*(f1) + pi*(1-p)*(f2) + (1-pi)*p*(f3) + (1-pi)*(1-p)*(f4)
-    summing = (torch.log(overall))
-    
-    return summing
-
-
-###############################################################################################
-###############################################################################################
-
-def get_gaussianloglikelihood_qw(x, mu, sigma, p):
-
-    return -0.5*np.log(2*np.pi) + torch.log( (1-p)/sigma*torch.exp(- (x)*(x)/(2 * sigma*sigma)) + (p)/sigma*torch.exp(- (x - mu)*(x - mu)/(2 * sigma*sigma) ))
-
-
-##############################################################################################
-# Overall likelihood, without the neural network part
-##############################################################################################
-# Here we are using the previous variational approximation as approximate posterior
-
-def first_likelihood(pi, mu_new, alpha_k, sigma_k, c, model, mu_prev, rho_prev, p, L):
-        
-    log_qw_theta_sum = 0
-    log_pw_sum       = 0
-    
-    for i in range(0, L-1):
-        sigma_weight = torch.log1p(torch.exp(model.rho[i].weight))
-        sigma_bias   = torch.log1p(torch.exp(model.rho[i].bias))
-
-        w_weight     = model.w[i].weight
-        w_bias       = model.w[i].bias
-
-        mu_weight    = model.mu[i].weight
-        mu_bias      = model.mu[i].bias
-
-        log_qw_theta_weight = get_gaussianloglikelihood_qw(w_weight, mu_weight, sigma_weight, p)
-        log_qw_theta_bias = get_gaussianloglikelihood_qw(w_bias, mu_bias, sigma_bias, p)
-
-        log_qw_theta_sum = log_qw_theta_sum + (log_qw_theta_weight).sum()+ (log_qw_theta_bias).sum()
-
-
-        sigma_prev_weight = torch.log1p(torch.exp(rho_prev[str(i)]["weight"]))
-        sigma_prev_bias   = torch.log1p(torch.exp(rho_prev[str(i)]["bias"]))
-
-        mu_prev_weight = mu_prev[str(i)]["weight"]
-        mu_prev_bias   = mu_prev[str(i)]["bias"]
-
-        mu_new_weight = mu_new[str(i)]["weight"]
-        mu_new_bias   = mu_new[str(i)]["bias"]    
-
-        log_pw_weight     = combined(w_weight, mu_prev_weight, sigma_prev_weight, alpha_k, sigma_k, mu_new_weight, pi, p, c)
-        log_pw_bias       = combined(w_bias, mu_prev_bias, sigma_prev_bias, alpha_k, sigma_k, mu_new_bias, pi, p, c)
-
-        log_pw_sum = log_pw_sum + (log_pw_weight).sum() + (log_pw_bias).sum()
-
-    return(log_qw_theta_sum - log_pw_sum)
-
-
-
-
-
-
-
-
 
 
 
